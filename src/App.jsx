2d9c969146e6ca9c4369c979da78ddbe2f8fe021
kwin64 <kwin11bvb@yandex.by>
2022-12-13 13:36:53 +0300

    feat: routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.scss';
+import Layout from './layout/Layout';
+import Post from './pages/post/Post';
+import Main from './pages/main/Main';
+import Posts from './pages/posts/Posts';
+import Login from './pages/login/Login';
+import Registration from './pages/registration/Registration';
+import NewPost from './pages/newPost/NewPost';
+import NotFound from './pages/notFound/NotFound';
+import EditPost from './pages/editPost/EditPost';
 
 function App() {
-  return <div className="App">dkfjck</div>;
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="posts" element={<Posts />} />
+        <Route path=":id" element={<Post />} />
+        <Route path=":id/edit" element={<EditPost />} />
+        <Route path=":new" element={<NewPost />} />
+        <Route path="login" element={<Login />} />
+        <Route path="registration" element={<Registration />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Layout>
+  );
 }
 
 export default App;
---
+++ src/pages/registration/Registration.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Registration = () => {
+  return <div>Reg</div>;
+};
+export default Registration;
