4fb68be926f9122fb93ab54cdaa3c35692559cc1
kwin64 <kwin11bvb@yandex.by>
2022-12-26 23:45:53 +0300

    change img container

--- client/src/utils/axios/index.js
+++ client/src/utils/axios/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const $api = axios.create({
-	// baseURL: process.env.REACT_APP_API_URL,
-	baseURL: 'https://kwin64-blog.herokuapp.com',
+	baseURL: process.env.REACT_APP_API_URL,
+	// baseURL: 'https://kwin64-blog.herokuapp.com',
 	withCredentials: true
 })
 
